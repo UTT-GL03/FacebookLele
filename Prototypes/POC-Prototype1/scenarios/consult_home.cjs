5ef0a4791f5ed1dddae36014a1e8e22d2e318467
Thomas_chbn <147448902+Darkman34@users.noreply.github.com>
2025-12-08 17:34:51 +0100

    Update consult_home

--- Prototypes/POC-Prototype1/scenarios/consult_home.cjs
+++ Prototypes/POC-Prototype1/scenarios/consult_home.cjs
@@ -2,8 +2,18 @@ const visit = async (page) => {
   await page.goto('', {
     waitUntil: 'networkidle',
   });
-  await page.waitForTimeout(10000);
-  await page.scrollToEnd();
+
+  await page.waitForTimeout(8000);
+
+  const scrollStep = 1000; // pixels à scroller par étape
+  const scrollTimes = 10;   // nombre de scrolls
+  for (let i = 0; i < scrollTimes; i++) {
+    await page.evaluate((step) => {
+      window.scrollBy(0, step);
+    }, scrollStep);
+    await page.waitForTimeout(1000);
+  }
+
   await page.waitForNetworkIdle();
   await page.waitForTimeout(7000);
 };
