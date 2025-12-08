9d769b9fce92968d9a820c92a0be7e481ab0a99b
Thomas_chbn <147448902+Darkman34@users.noreply.github.com>
2025-12-08 15:30:57 +0100

    greenframe + séance 9

---
+++ Prototypes/POC-Prototype1/scenarios/consult_post.js
@@ -0,0 +1,12 @@
+// Scénario : Consulter un post spécifique
+const visit = async (page) => {
+  await page.goto('http://host.docker.internal/post/26', {
+    waitUntil: 'networkidle',
+  });
+  await page.waitForTimeout(10000);
+  await page.scrollToEnd();
+  await page.waitForNetworkIdle();
+  await page.waitForTimeout(7000);
+};
+
+module.exports = visit;
