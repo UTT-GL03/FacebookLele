721f71f901f471a30814f6613c3a2333489d20d9
Thomas_chbn <147448902+Darkman34@users.noreply.github.com>
2025-12-12 00:32:00 +0100

    Séance 10, point 3 (chargement de 20 doc les plus récent)

--- Prototypes/POC-Prototype1/src/HomePage.jsx
+++ Prototypes/POC-Prototype1/src/HomePage.jsx
@@ -5,11 +5,21 @@ function HomePage() {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:5984/facebooklele_database/_all_docs?include_docs=true')
+    //fetch('http://localhost:5984/facebooklele_database/_all_docs?include_docs=true')
+    fetch('http://localhost:5984/facebooklele_database/_find', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          selector: { date: { "$gt": null } },
+          sort: [{ date: "desc" }],
+          fields: [ "_id", "title", "author", "date", "id", "content", "commentsSection" ],
+          limit: 20
+        })
+    })
       .then(response => response.json())
       .then(data => {
-        const posts = data.rows.map(row => row.doc)
-        setPosts(posts)
+        //const posts = data.rows.map(row => row.doc)
+        setPosts(data.docs)
       })
       .catch(error => {
         console.error('Erreur lors du chargement des données:', error)
