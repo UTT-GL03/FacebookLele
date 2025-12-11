d441ee5ef8eeba469d391fc497e8a033d5fc38e2
Thomas_chbn <147448902+Darkman34@users.noreply.github.com>
2025-12-12 00:53:10 +0100

    Version v2.0.1

--- Prototypes/POC-Prototype1/src/HomePage.jsx
+++ Prototypes/POC-Prototype1/src/HomePage.jsx
@@ -3,6 +3,8 @@ import Post from './Post.jsx'
 
 function HomePage() {
   const [posts, setPosts] = useState([])
+  const [nextBookmark, setNextBookmark] = useState()
+  const [requestedBookmark, setRequestedBookmark] = useState()
 
   useEffect(() => {
     //fetch('http://localhost:5984/facebooklele_database/_all_docs?include_docs=true')
@@ -13,24 +15,38 @@ function HomePage() {
           selector: { date: { "$gt": null } },
           sort: [{ date: "desc" }],
           fields: [ "_id", "title", "author", "date", "id", "content", "commentsSection" ],
+          bookmark: requestedBookmark,
           limit: 20
         })
     })
       .then(response => response.json())
       .then(data => {
-        //const posts = data.rows.map(row => row.doc)
-        setPosts(data.docs)
+
+        // Ajouter les nouveaux posts aux posts existants
+        setPosts(prevPosts => [...prevPosts, ...data.docs])
+        // Sauvegarder le bookmark pour la prochaine page
+        setNextBookmark(data.bookmark)
       })
       .catch(error => {
         console.error('Erreur lors du chargement des données:', error)
       })
-  }, [])
+  }, [requestedBookmark])
 
   return (
     <>
       {posts.map((p) => (
         <Post key={p._id} post={p} />
       ))}
+      
+      {nextBookmark && (
+        <button 
+          type="button" 
+          onClick={() => setRequestedBookmark(nextBookmark)}
+          className="load-more-btn"
+        >
+          Charger plus de posts
+        </button>
+      )}
     </>
   )
 }
